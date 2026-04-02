6fcb35a4dabca284320a33bc81a736b46b5f94e9
김예령 <yaeryung93@gmail.com>
2026-04-02 21:28:09 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        double a=num1;
+        double b=num2;
+        
+        answer=(int)(a/b*1000);
+        return answer;
+    }
+}
