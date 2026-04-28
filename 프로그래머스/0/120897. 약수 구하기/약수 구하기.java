14c8081f37a3fac09845403455a6884e8c05837b
예령 <yaeryung93@gmail.com>
2026-04-28 09:32:09 +0900

    [level 0] Title: 약수 구하기, Time: 0.30 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] solution(int n) {
         int a=0;
-        int b=0;
         for(int i=1; i<=n; i++){
             if(n%i==0){
                 a++;
             }
         }
         int[] answer = new int[a];
+        a=0;
          for(int j=1; j<=n; j++){
             if(n%j==0){
-                answer[b]=j;
-                b++;
+                answer[a]=j;
+                a++;
             }
         }
         return answer;
