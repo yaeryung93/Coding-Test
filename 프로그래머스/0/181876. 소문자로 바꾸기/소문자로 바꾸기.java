c5a83fbdb213ebcc73188399ce494c694168f6b8
예령 <yaeryung93@gmail.com>
2026-05-12 12:05:11 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 9.11 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i=0; i<myString.length(); i++){
+            char a=myString.charAt(i);
+            if(a<97){
+                answer+=(char)(a+32);
+            }
+            else{
+                answer+=(char)a;
+            }
+        }
+        return answer;
+    }
+}
