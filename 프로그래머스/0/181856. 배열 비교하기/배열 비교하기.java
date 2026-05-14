6d14561070bb4ab2c394dcef242c94cf9d523f8a
예령 <yaeryung93@gmail.com>
2026-05-14 13:50:35 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int sum1=0;
+        int sum2=0;
+        for(int i=0; i<arr1.length; i++){
+            sum1+=arr1[i];
+        }
+        for(int i=0; i<arr2.length; i++){
+            sum2+=arr2[i];
+        }
+        
+        if(arr1.length==arr2.length){
+            if(sum1>sum2) answer=1;
+            else if(sum2>sum1) answer=-1;
+        }
+        else{
+            if(arr1.length>arr2.length) answer=1;
+            else if(arr1.length<arr2.length) answer=-1;
+        }
+        return answer;
+    }
+}
