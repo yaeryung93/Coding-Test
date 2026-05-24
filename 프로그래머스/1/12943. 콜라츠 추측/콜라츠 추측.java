78639abb0121ee4a64c50bb85363910290d7fe5a
예령 <yaeryung93@gmail.com>
2026-05-24 22:36:47 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(long num) {
+        if(num==1) return 0;
+        for(int i=1; i<=500; i++){
+            if(num%2==0){
+                num/=2;
+            }
+            else{
+                num=(num*3)+1;
+            }
+            if(num==1) return i;
+        }
+        return -1;
+    }
+}
