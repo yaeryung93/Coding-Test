1d24a698e215a5467af3bd95807cebf21257e30e
예령 <yaeryung93@gmail.com>
2026-06-01 21:57:22 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.11 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int max=numbers[0]*numbers[1];
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                if(max<numbers[i]*numbers[j])
+                    max=numbers[i]*numbers[j];
+            }
+        }
+        return max;
+    }
+}
