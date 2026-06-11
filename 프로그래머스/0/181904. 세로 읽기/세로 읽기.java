2fad33f9db94e48c352cf95d35fd0d5e9e4073da
예령 <yaeryung93@gmail.com>
2026-06-11 21:52:03 +0900

    [level 0] Title: 세로 읽기, Time: 4.77 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        
+        if(m==1) return my_string;
+        
+        String answer = "";
+
+        for(int i=c-1; i<my_string.length(); i+=m){
+            answer+=my_string.charAt(i);
+        }
+        return answer;
+    }
+}
