467d1076d6416028797caed73952dcf19dc2bfa5
예령 <yaeryung93@gmail.com>
2026-07-10 08:18:54 +0900

    [level 0] Title: 문자열 뒤집기, Time: 6.13 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,9 +2,6 @@ class Solution {
     public String solution(String my_string, int s, int e) {
         String str=my_string.substring(s,e+1);
         StringBuilder rever=new StringBuilder(str);
-        String answer=(my_string.substring(0,s)
-                 +rever.reverse().toString()
-                       +my_string.substring(e+1));
-        return answer;
+        return my_string.substring(0,s)+rever.reverse().toString()+my_string.substring(e+1);
     }
 }
