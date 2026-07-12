6fac4465745dec743605288e328679173693938e
예령 <yaeryung93@gmail.com>
2026-07-13 00:09:15 +0900

    [level 0] Title: 배열 만들기 6, Time: 17.79 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk=new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            if(stk.isEmpty()){
+                stk.add(arr[i]);
+            }
+            else{
+                if(stk.get(stk.size()-1)==arr[i]){
+                    stk.remove(stk.size()-1);
+                }
+                else{
+                    stk.add(arr[i]);
+                }
+            }
+        }
+        if(stk.isEmpty()){
+            int[] answer={-1};
+            return answer;
+        }
+        int[] answer=new int[stk.size()];
+        for(int i=0; i<answer.length; i++)
+            answer[i]=stk.get(i);
+        return answer;
+    }
+}
