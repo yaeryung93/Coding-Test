e93a2069c5ddfcca01f91416b3d3785359de20a7
예령 <yaeryung93@gmail.com>
2026-07-19 23:13:38 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int[] count=new int[201];
+
+        for (int i=0; i<lines.length; i++) {
+            int start = lines[i][0];
+            int end = lines[i][1];
+
+            for (int j = start; j < end; j++) {
+                count[j+100]++;
+            }
+        }
+
+        for (int  i=0; i<count.length; i++) {
+            if (count[i]>=2) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
