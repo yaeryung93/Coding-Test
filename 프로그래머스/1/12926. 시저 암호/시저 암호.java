e7c79090c016578da98bf783832bcb2fd400fca4
예령 <yaeryung93@gmail.com>
2026-07-22 09:51:36 +0900

    [level 1] Title: 시저 암호, Time: 22.55 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,18 +1,17 @@
 class Solution {
     public String solution(String s, int n) {
-        String num="";
         for(int i=1; i<=n; i++){
-            num="";
+            String temp="";
             for(int j=0; j<s.length(); j++){
                 if(s.charAt(j)==' ')
-                    num+=s.charAt(j);
+                    temp+=s.charAt(j);
                 else if(s.charAt(j)=='Z'||s.charAt(j)=='z')
-                    num+=(char)(s.charAt(j)-25);
+                    temp+=(char)(s.charAt(j)-25);
                 else
-                    num+=(char)(s.charAt(j)+1);
+                    temp+=(char)(s.charAt(j)+1);
             }
-            s=num;
+            s=temp;
         }
-        return num;
+        return s;
     }
 }
