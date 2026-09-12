bdeae04e3976acfd0b491c387a62b435392920c1
예령 <yaeryung93@gmail.com>
2026-09-12 20:45:27 +0900

    [level 1] Title: 과일 장수, Time: 33.68 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        Arrays.sort(score);
+        int answer = 0;
+        for (int i=score.length%m; i<score.length; i+=m) {
+            answer+=score[i];
+        }
+        return m*answer;
+        
+    }
+}
